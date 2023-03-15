7634bb5c65f988d5d3e5b98a135d21e73935f612
Anita <52679613+anitarestrepo16@users.noreply.github.com>
2023-03-15 15:50:17 -0500

    need to tidy up

--- Mindware Compilers/HRV_compiler.R
+++ Mindware Compilers/HRV_compiler.R
@@ -13,5 +13,10 @@ files = list(list.files(files_dir))
 for(file in files){
   id = str_replace(file, ".xlsx", "")
   df = read_excel(paste0(files_dir, file)) %>%
-    filter(.[1] %in% vars_to_keep)
+    filter(`Segment Number` %in% vars_to_keep)
+  df2 = as_tibble(t(df))[2:nrow(df2), ]
+  names(df2) = vars_to_keep
+  df2$Segment = c(seq(1, nrow(df2)))
+  df2$id = id
+  
 }
