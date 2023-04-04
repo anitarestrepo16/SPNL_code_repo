ecc0dc0d371b1b49695e50a615988bc1e03205f7
Anita <52679613+anitarestrepo16@users.noreply.github.com>
2023-04-04 10:26:22 -0500

    added EDA compiler

---
+++ Mindware Compilers/EDA_compiler.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(readxl)
+
+
+compile <- function(directory = getwd(), vars_to_keep = c("Segment Number", "Start Event Name", "Start Time", "End Event Name", "End Time", "Total SCRs", "ER-SCRs", "NS-SCRs", "Tonic SCL", "Mean SC")) {
+  directory = paste0(directory, "/")
+  # obtain the excel file names
+  files = list(list.files(directory, pattern = "*.xlsx$"))
+  
+  # extract data for each excel file
+  df_out <- data.frame(matrix(ncol = (length(vars_to_keep) + 1), nrow = 0))
+  colnames(df_out) <- c(vars_to_keep, "ID")
+  for(file in files){
+    # save file name as id 
+    ID <- str_replace(file, ".xlsx", "")
+    # read in the file
+    df1 <- read_excel(paste0(directory, file)) 
+    # filter the file
+    df2 <- df1[40:nrow(df1), ] %>%
+      # keep only relevant variables
+      filter(Version %in% vars_to_keep)
+    # transpose the df
+    df3 <- as_tibble(t(df2))[3:nrow(t(df2)), ]
+    # change column names
+    names(df3) <- vars_to_keep
+    # add id column
+    df3$ID <- ID
+    # append the subject data to the final df
+    df_out = rbind(df_out, df3)
+  }
+  return(df_out)
+}
+
